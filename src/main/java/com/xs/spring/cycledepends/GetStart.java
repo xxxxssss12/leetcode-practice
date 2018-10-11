3d53558809972896450a271d82a2038d1f6acc42
xiongshun <xiongshun@wecash.net>
2018-10-11 18:59:09 +0800

    over

--- src/main/java/com/xs/spring/cycledepends/GetStart.java
+++ src/main/java/com/xs/spring/cycledepends/GetStart.java
@@ -2,9 +2,6 @@ package com.xs.spring.cycledepends;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * Created by xs on 2018/9/10
@@ -12,9 +9,9 @@ import java.util.List;
 public class GetStart {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanA.class, BeanB.class);
-        BeanA beanA = applicationContext.getBean(BeanA.class);
+        BeanA beanA = (BeanA) applicationContext.getBean("beanA");
         beanA.test();
-        BeanB beanB = applicationContext.getBean(BeanB.class);
+        BeanB beanB = (BeanB) applicationContext.getBean("beanB");
         beanB.test();
 
     }
