0fef793e275c218f3829023895f55a22b8620dfc
xiongshun <xiongshun@wecash.net>
2019-01-31 10:48:54 +0800

    1

--- src/main/java/com/xs/rocketmq/v4/SyncProducer.java
+++ src/main/java/com/xs/rocketmq/v4/SyncProducer.java
@@ -14,12 +14,12 @@ public class SyncProducer {
         DefaultMQProducer producer = new
                 DefaultMQProducer("please_rename_unique_group_name");
         // Specify name server addresses.
-        producer.setNamesrvAddr("localhost:9876");
+        producer.setNamesrvAddr("");
         //Launch the instance.
         producer.start();
         for (int i = 0; i < 100; i++) {
             //Create a message instance, specifying topic, tag and message body.
-            Message msg = new Message("TopicTest" /* Topic */,
+            Message msg = new Message("xs-topic-test" /* Topic */,
                     "TagA" /* Tag */,
                     ("Hello RocketMQ " +
                             i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
