e34fe0dddf887fd7a45ffd05760bd18de97dd4b3
xxxxssss12 <xs6666@qq.com>
2019-04-12 22:28:19 +0800

    1

---
+++ src/main/java/com/xs/other/ThreadPoolTest.java
@@ -0,0 +1,11 @@
+package com.xs.other;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolTest {
+    private static ExecutorService executor = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+    }
+}
--- src/main/java/com/xs/other/jse/FixThreadPoolTest.java
+++ src/main/java/com/xs/other/jse/FixThreadPoolTest.java
@@ -4,45 +4,44 @@ package com.xs.other.jse;
 import org.springframework.util.StopWatch;
 
 import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by xs on 2018/5/9
  */
 public class FixThreadPoolTest {
-    static Executor executor = Executors.newFixedThreadPool(2);
+    static Executor executor;
 
     public static void main(String[] args) throws InterruptedException {
-        HashMap map;
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start("test1");
-        Thread.sleep(5000);
-        stopWatch.stop();
-
-        stopWatch.start("test2");
-        Thread.sleep(3000);
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
+//        HashMap map;
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start("test1");
+//        Thread.sleep(5000);
+//        stopWatch.stop();
+//
+//        stopWatch.start("test2");
+//        Thread.sleep(3000);
+//        stopWatch.stop();
+//        System.out.println(stopWatch.prettyPrint());
 
 //        ConcurrentHashMap map;
-//        for (int i=0; i<10;i++) {
-//            executor.execute(new Runnable() {
-//                @Override
-//                public void run() {
-//                    try {
-//                        System.out.println("thread=" + Thread.currentThread().getName() + ";执行开始！");
-//                        Thread.sleep(5000);
-//                        System.out.println("thread=" + Thread.currentThread().getName() + ";执行完毕！");
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            });
-//            System.out.println("i=" + i + "execute执行完毕！！");
-//        }
-//        Thread.sleep(1000000);
+        int arg1=2;//核心线程
+        int arg2=40;//最大线程数量
+        int arg3=100;//空余保留时间
+        executor = new ThreadPoolExecutor(arg1, arg2, arg3,TimeUnit.MILLISECONDS, // 时间单位
+                new LinkedBlockingQueue<Runnable>());//默认构造的队列大小为Integer.Max, 可指定大小new LinkedBlockingQueue<Runnable>(3)，队列容量为3
+        for (int i=0; i<10;i++) {
+            executor.execute(() -> {
+                try {
+                    System.out.println("thread=" + Thread.currentThread().getName() + ";执行开始！");
+                    Thread.sleep(5000);
+                    System.out.println("thread=" + Thread.currentThread().getName() + ";执行完毕！");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            System.out.println("i=" + i + "execute执行完毕！！");
+        }
+        Thread.sleep(1000000);
     }
 }
