cc436beb2bba7a4a431612dd410ea1f1f4e06781
xxxxssss12 <xs6666@qq.com>
2019-04-26 08:38:07 +0800

    1

--- src/main/java/com/xs/Utils.java
+++ src/main/java/com/xs/Utils.java
@@ -8,11 +8,11 @@ import java.util.Random;
  * Created by xs on 2019/4/12
  */
 public class Utils {
-    public static int[] createNums(int n) {
+    public static int[] createNums(int n, int bound) {
         Random ran = new Random();
         int[] arr = new int[n];
         for (int i=0; i<n; i++) {
-            arr[i] = ran.nextInt(20) - 10;
+            arr[i] = ran.nextInt(bound) - (bound/2);
         }
         System.out.println(JSON.toJSONString(arr));
         return arr;
---
+++ src/main/java/com/xs/other/sorts/quicksort/Practice.java
@@ -0,0 +1,50 @@
+package com.xs.other.sorts.quicksort;
+
+import com.alibaba.fastjson.JSON;
+import com.xs.Utils;
+
+public class Practice {
+    public static void main(String[] args) {
+        int[] nums = Utils.createNums(10, 100);
+        System.out.println(JSON.toJSONString(nums));
+        quickSort(nums, 0, nums.length - 1);
+        System.out.println(JSON.toJSONString(nums));
+    }
+
+    private static void quickSort(int[] nums, int start, int end) {
+        int low = start;
+        int high = end;
+        int base = nums[low];
+        boolean exchange = false;
+        while (high > low) {
+            exchange = false;
+            while (high > low) {
+                if (nums[high] < base) {
+                    exchange = true;
+                    break;
+                }
+                high--;
+            }
+            if (exchange) {
+                int tmp = nums[high];
+                nums[high] = nums[low];
+                nums[low] = tmp;
+            }
+            exchange = false;
+            while (low < high) {
+                if (nums[low] > base) {
+                    exchange = true;
+                    break;
+                }
+                low++;
+            }
+            if (exchange) {
+                int tmp = nums[low];
+                nums[low] = nums[high];
+                nums[high] = tmp;
+            }
+        }
+        if (start < low - 1) quickSort(nums, start, low - 1);
+        if (end > low + 1) quickSort(nums, low + 1, end);
+    }
+}
