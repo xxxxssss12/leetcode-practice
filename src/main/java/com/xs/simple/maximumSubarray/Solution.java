eaf54fc73c1beb0329fd831ebabe6e414d2edf80
xs <xs6666@qq.com>
2020-04-28 19:43:04 +0800

    1

--- src/main/java/com/xs/simple/maximumSubarray/Solution.java
+++ src/main/java/com/xs/simple/maximumSubarray/Solution.java
@@ -9,57 +9,36 @@ import java.util.Random;
  */
 public class Solution {
 //    private static int[] nums = null;
-    public int maxSubArray(int[] nums) {
-//        if (Solution.nums == null) {
-//            Solution.nums = nums;
-//        }
-        int max = getMax(nums, nums.length);
-        return max;
-    }
 
-    private int getMax(int[] nums, int n) {
-        if (n == 1) {
-            return nums[0];
-        }
-        int max = Integer.MIN_VALUE;
-        int[] solution = new int[n];
-        for (int i=0; i<nums.length; i++) {
-            if (i == 0) {
-                solution[i] = nums[i];
-            } else {
-                solution[i] = Math.max(solution[i-1] + nums[i], nums[i]);
-            }
-            max = Math.max(max, solution[i]);
-        }
-        return solution[n - 1];
-//        int a1 = getMax(childLength-1, index);
-//        int a2 = getMax(childLength-1, index+1);
-//        int a3 = a1 + nums[childLength-1];
-//        int a4 = a2 + nums[index];
-//        System.out.println("now=(" + childLength + "," + index + ");" + "\t" +
-//                "("+(childLength-1)+","+index+")=" + a1 + ";" + "\t" +
-//                "("+(childLength-1)+","+(index+1)+")=" + a2 + ";" + "\t" +
-//                "("+(childLength-1)+","+index+")+a("+(childLength-1)+")=" + a3 + ";" + "\t" +
-//                "("+(childLength-1)+","+(index+1)+")+a("+(index)+")=" + a4);
-//        return max(a1, a2, a3, a4);
-    }
 
-    private int max(int a1, int a2, int a3, int a4) {
-        int max = a1;
-        max = Math.max(max, a2);
-        max = Math.max(max, a3);
-        max = Math.max(max, a4);
-        return max;
+    private int max(int a1, int a2) {
+        return Math.max(a1, a2);
     }
 
     public static void main(String[] args) {
-        int[] nums = createNums();
+        int[] nums = createNums(10);
         System.out.println(new Solution().maxSubArray(nums));
     }
 
-    private static int[] createNums() {
+    private int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int[] dp = new int[nums.length];
+        for (int i=0; i<nums.length; i++) {
+            if (i==0) {
+                dp[i] = nums[i];
+                max = dp[i];
+                continue;
+            }
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+
+    private static int[] createNums(int n) {
         Random ran = new Random();
-        int n = 6;
         int[] arr = new int[n];
         for (int i=0; i<n; i++) {
             arr[i] = ran.nextInt(20) - 10;
