8a8a4ddbcf8b250d0eaf4c3ef88566f88e8e4603
xxxxssss12 <xs6666@qq.com>
2020-06-22 23:01:47 +0800

    1

--- src/main/java/com/xs/other/transmit/threadlocal/TransmitableThreadLocalTest.java
+++ src/main/java/com/xs/other/transmit/threadlocal/TransmitableThreadLocalTest.java
@@ -1,7 +1,9 @@
 package com.xs.other.transmit.threadlocal;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
+import com.alibaba.ttl.threadpool.TtlExecutors;
 import com.xs.MyThreadFactory;
+import com.xs.Utils;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,22 +12,51 @@ import java.util.concurrent.Executors;
  * Created by xs on 2019/4/29
  */
 public class TransmitableThreadLocalTest {
-    private static ExecutorService executor = Executors.newFixedThreadPool(10, new MyThreadFactory());
+    private static ExecutorService executor = TtlExecutors.getTtlExecutorService(Utils.generatorExecutor(10, "a"));
 
 //    public static final TransmittableThreadLocal<String> context = new TransmittableThreadLocal<String>();
 //    public static final ThreadLocal<String> context = new ThreadLocal<String>();
-    public static final InheritableThreadLocal<String> context = new InheritableThreadLocal<String>();
+    public static final TransmittableThreadLocal<String> context = new TransmittableThreadLocal<String>();
     public static void main(String[] args) throws InterruptedException {
         TransmitableThreadLocalTest test = new TransmitableThreadLocalTest();
-        int i =0;
-        while (true) {
-            context.set("test-" + (++i));
-            System.out.println(context.get());
-            for (int j = 0; j< 20; j++) {
-                executor.execute(() -> System.out.println(Thread.currentThread().getName() + "...context=" + context.get()));
+
+        new Thread(() -> {
+            int i =0;
+            for (;;) {
+                context.set("t1-" + (++i));
+                System.out.println(context.get());
+                for (int j = 0; j < 10; j++) {
+                    executor.execute(() -> {
+                        System.out.println(Thread.currentThread().getName() + "...context=" + context.get());
+                    });
+                }
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                context.remove();
             }
-            Thread.sleep(10000);
-            context.remove();
-        }
+        }).start();
+
+        new Thread(() -> {
+            int i =10000;
+            for (;;) {
+                context.set("t2-" + (++i));
+                System.out.println(context.get());
+                for (int j = 0; j < 10; j++) {
+                    executor.execute(() -> {
+                        System.out.println(Thread.currentThread().getName() + "...context=" + context.get());
+                    });
+                }
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                context.remove();
+            }
+        }).start();
+        Thread.interrupted();
     }
 }
